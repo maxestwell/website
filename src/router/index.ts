d5a68142008476e4f79a1019c9a222d8bf41314d
Ben Ennis Butler <ben@beneb.com>
2024-11-07 10:39:50 +1100

    updates for max

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PortfolioPage from '../views/PortfolioPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,8 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    { path: '/portfolio/:id', component: PortfolioPage, props: true },
+
     {
       path: '/about',
       name: 'about',
